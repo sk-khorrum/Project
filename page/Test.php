f93465c6dd8ec02511558d5915250138331baeef
DRT <91899053+sk-khorrum@users.noreply.github.com>
2026-02-20 06:35:36 +0600

    Create Test.php

---
+++ page/Test.php
@@ -0,0 +1,20 @@
+POST /backend/api/v1/otp HTTP/1.1
+Host: weblogin.grameenphone.com
+Connection: keep-alive
+Content-Length: 24
+sec-ch-ua-platform: "Android"
+User-Agent: Mozilla/5.0 (Linux; Android 13; V2037 Build/TP1A.220624.014) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/144.0.7559.132 Mobile Safari/537.36
+Accept: application/json, text/plain, */*
+sec-ch-ua: "Not(A:Brand";v="8", "Chromium";v="144", "Android WebView";v="144"
+Content-Type: application/json
+sec-ch-ua-mobile: ?1
+Origin: https://weblogin.grameenphone.com
+X-Requested-With: mark.via.gp
+Sec-Fetch-Site: same-origin
+Sec-Fetch-Mode: cors
+Sec-Fetch-Dest: empty
+Referer: https://weblogin.grameenphone.com/
+Accept-Encoding: gzip, deflate, br, zstd
+Accept-Language: en-GB,en-US;q=0.9,en;q=0.8
+
+{"msisdn":"01314089563"}
